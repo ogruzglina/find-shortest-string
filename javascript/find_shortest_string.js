a583c6446952edde138e5eae35f6598c16c0cc49
Olha Gruzglina <olga.zhmura@gmail.com>
2022-04-12 11:19:53 -0400

    commit tree fcd678c2c2726a8e4d29521a9c3ae639b31aa536

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,9 +1,27 @@
 function findShortestString(arr) {
   // type your code here
+  //if arr length is 1 then return length of that element
+  if (arr.length === 1) {
+    return arr[0];
+  }
+ let min = arr[0];
+  //go through all elem in arr and compare length 
+  // if arr[i].length less than min then min = arr[i]
+  for (let i = 1; i < arr.length; i++) {
+    if (min.length > arr[i].length) {
+      min = arr[i];
+    }
+  }
+  //return min
+  return min;
 }
 
 if (require.main === module) {
   // add your own tests in here
+  console.log("Expecting: 'aaa'");
+  console.log("=>", findShortestString(['aaa']));
+
+  console.log("");
   console.log("Expecting: 'a'");
   console.log("=>", findShortestString(['aaa', 'a', 'bb', 'ccc']));
 
